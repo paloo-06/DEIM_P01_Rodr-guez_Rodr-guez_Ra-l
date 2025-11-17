7e874c5f0e3da2c8cb866f767ecd276cec772998
Raúl <r.rodriguez.alum@fp.mercedarias.es>
2025-11-17 16:04:51 +0100

    Sistema de puntuación

--- prueba - copia (2)/Assets/script/Prota.cs
+++ prueba - copia (2)/Assets/script/Prota.cs
@@ -1,8 +1,11 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 [RequireComponent (typeof(Rigidbody2D))]
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    [SerializeField]  //para mostrar algo en el impector que esta oculto 
+    private int bestscore; //variable para tener la maxima puntuación
     public TMP_Text Marcadorpuntos;
     public static int Puntos =5;
     Rigidbody2D rb;
@@ -17,27 +20,32 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //condicion que indica que el persoanje se muecve pa lña derecha
+       // condicion que indica que el persoanje se muecve pa lña derecha
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-           // transform.Translate(velocidad * Time.deltaTime, 0, 0);
+             transform.Translate(velocidad * Time.deltaTime, 0, 0);
            rb.linearVelocityX = velocidad;
 
         }
-        else if (Input.GetKeyUp (KeyCode.D))//detecta que ya no esta tocando la tecla
+        else if (Input.GetKey(KeyCode.D))//detecta que ya no esta tocando la tecla
         {
-            rb.linearVelocityX= 0; // cuando ya no lo esta tocando y es 0
+            rb.linearVelocityX = 0; // cuando ya no lo esta tocando y es 0
         }
 
         if (Input.GetKeyDown(KeyCode.A))
-        {
-            // transform.Translate(-velocidad * Time.deltaTime, 0, 0);
-            rb.linearVelocityX = -velocidad;
+       {
+             transform.Translate(-velocidad * Time.deltaTime, 0, 0);
+           rb.linearVelocityX = -velocidad;
         }
-        else if (Input.GetKeyUp(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             rb.linearVelocityX = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            checkbestscore();
+
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -59,4 +67,16 @@ public class NewMonoBehaviourScript : MonoBehaviour
             Marcadorpuntos.text = ":" + Puntos;
         }
     }
-}
+    private void checkbestscore() 
+    {//para guardar la maxikma puntuacion de manera local no de manera online 
+        if(Puntos >=PlayerPrefs.GetInt("bestscore"))
+        {
+            //Guarda la maxima puntiacion en el computador 
+            PlayerPrefs.SetInt("bestscore", Puntos);
+            //bestscore = Puntos;
+        }
+    
+    }
+
+    
+} 
